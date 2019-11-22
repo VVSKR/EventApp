dbe0bf060a9e8cb3193476b6e07489ebe67e1bd5
Vova SKR <vova.skri.dev@gmail.com>
2019-11-22 15:32:00 +0300

    Initial Commit

---
+++ EventApp 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventApp 1
+//
+//  Created by Vova SKR on 22/11/2019.
+//  Copyright © 2019 Vova SKR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
